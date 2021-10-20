2f66202396d5dbd829df2212524a0afbec300a5b
lalu jaya satria permana <jayasatria9@gmail.com>
2021-10-20 19:36:38 +0800

    ambil data
    belum fix

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,7 +8,7 @@ class Home extends BaseController
 {
     public function index()
     {
-        $this->db->where('id' > 9);
+        $this->db->where('id' < 9);
         $data['emasMini'] = $this->db->get('emasMini')->result_array();
         $data['title'] = "EmasKu";
 
